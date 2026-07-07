c4923362c9ec5b8d9ac090d4136e5df46bfc7f68
JaeanHan <jaean1999@naver.com>
2026-07-07 10:50:57 +0900

    init: 프로젝트 생성

---
+++ chat-server/src/test/java/com/jaeano/chat_server/TestChatServerApplication.java
@@ -0,0 +1,11 @@
+package com.jaeano.chat_server;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestChatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ChatServerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
